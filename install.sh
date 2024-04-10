d9fa18a231e6ad77f43e33b0c4e808a06ddf6b6b
Chris Hooke <chooke@unca.edu>
2024-04-10 13:12:27 -0400

    removed exec

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 # End Nix
-exec PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH"
+PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH"
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
