2c28f442ab5e903757bb5488b6ac0d7dd2083b85
Chris Hooke <chooke@unca.edu>
2024-04-11 09:21:14 -0400

    added search and replace for auto generated config

--- install.sh
+++ install.sh
@@ -13,4 +13,7 @@ nix-channel --add https://github.com/nix-community/home-manager/archive/master.t
 nix-channel --update
 nix-shell '<home-manager>' -A install
 cd ~/.config/home-manager && { curl -O https://raw.githubusercontent.com/BACONWRAP/porta-env/main/home.nix ; cd -; }
+sed -i.bak 's/home.username.*/home.username = "${(whoami)}";/g' ~/.config/home-manager/home.nix && rm ~/.config/home-manager/home.nix.bak
+sed -i.bak 's/home.username.*/home.username = "${(echo $HOME)}";/g' ~/.config/nixpkgs/home.nix && rm ~/.config/nixpkgs/home.nix.bak
+sed 
 home-manager switch
